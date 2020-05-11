2ef80668b647993e252fbda5254d9ac493b120dc
eminshamshoian <eminshamshoian@Emins-MacBook-Pro.local>
2020-05-10 21:17:35 -0700

    created initial nav and active links

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Signup from './components/user/Signup';
 import Signin from './components/user/Signin';
 import Home from './components/core/Home';
+import Navbar from './components/layout/Navbar';
 
 const Routes = () => {
     return (
-        <Router>
+        <BrowserRouter>
+            <Navbar />
             <Switch>
                 <Route path='/' exact component={Home} />
                 <Route path='/signup' exact component={Signup} />
                 <Route path='/signin' exact component={Signin} />
             </Switch>
-        </Router>
+        </BrowserRouter>
     );
 };
 
